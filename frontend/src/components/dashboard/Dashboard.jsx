3644b41ab9347733388079d4e9584f80efb7c030
eoinmoney78 <94717367+eoinmoney78@users.noreply.github.com>
2023-08-19 20:38:25 -0400

    design colors login reg dashboard

--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom'; // Outlet is used to render nested routes
+import { Outlet } from 'react-router-dom';
+import './dashboard.css';
+import { Container, Row, Col, Card, CardBody } from 'reactstrap';
+import Logout from '../auth/logout/Logout'; // Import the Logout component
 
 function Dashboard({ token, isAdmin }) {
-  // You can use the 'token' and 'isAdmin' props to conditionally render content
-
   return (
-    <div>
-      <h2>Welcome to the Dashboard!</h2>
-      {isAdmin && <p>You have admin privileges.</p>}
-      <Outlet /> {/* Render nested routes within the Dashboard */}
-    </div>
+    <Container className="dashboard-container">
+      <Row>
+        <Col>
+          <Card className="dashboard-card">
+            <CardBody className="text-center">
+              <h2 className="dashboard-heading">Welcome to the Dashboard!</h2>
+              {isAdmin && <p className="admin-message">You have admin privileges.</p>}
+              <Outlet />
+              {/* Include the Logout component */}
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+      <Logout /> 
+    </Container>
+    
   );
 }
 
