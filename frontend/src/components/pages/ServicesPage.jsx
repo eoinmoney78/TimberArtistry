d0e0a6e2f42ee3c8e6829326a0131e39308305f6
eoinmoney78 <94717367+eoinmoney78@users.noreply.github.com>
2023-08-23 16:48:52 -0400

    services

--- frontend/src/components/pages/ServicesPage.jsx
+++ frontend/src/components/pages/ServicesPage.jsx
@@ -4,69 +4,71 @@ import { Typography, Container, Box, Grid, Card, CardContent, CardActionArea, Bu
 import {BiCabinet} from  'react-icons/bi';
 import {GiPaintBrush} from 'react-icons/gi';
 import {GiDeadWood} from 'react-icons/gi';
+import './servicespage.css';
 
 function ServicesPage() {
   return (
-    <Container>
-<Box height="35vh" display="flex" flexDirection="column" justifyContent="center" paddingTop="100px">
-    <Typography variant="h2" align="center" gutterBottom>
+    <Container className="services-page">
+      <Box height="40vh" display="flex" flexDirection="column" justifyContent="center" paddingTop="100px">
+      <Typography variant="h2" align="center" gutterBottom>
       Our Services
-    </Typography>
-</Box>
+       </Typography>
+     </Box>
 
 
       <Grid container spacing={2}>
-        
-        <Grid item xs={12} md={4}>
-          <Card variant="outlined" elevation={4} sx={{ "&:hover": { transform: "scale(1.05)", transition: "0.3s" } }}>
-            <CardActionArea>
-              <CardMedia component={BiCabinet} height="140" />
-              <CardContent>
-                <Typography variant="h5" align="center" gutterBottom>
-                  Cabinet Re-facing
-                </Typography>
-                <Typography align="center" paragraph>
-                  Give your cabinets a fresh, modern look without the hassle of a full remodel.
-                </Typography>
-                <Button variant="contained" color="primary" fullWidth>Learn More</Button>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
+      <Grid item xs={12} md={4}>
+      <Card variant="outlined" elevation={4} sx={{ "&:hover": { transform: "scale(1.05)", transition: "0.3s" } }}>
+        <CardActionArea>
+          <CardMedia component={BiCabinet} height="200" /> {/* Increased height */}
+          <CardContent sx={{ padding: '24px' }}> {/* Increased padding */}
+            <Typography variant="h4" align="center" gutterBottom> {/* Increased font size */}
+              Cabinet Re-facing
+            </Typography>
+            <Typography align="center" paragraph style={{ fontSize: '18px' }}> {/* Increased font size */}
+              Give your cabinets a fresh, modern look without the hassle of a full remodel.
+            </Typography>
+            <Button variant="contained" color="primary" fullWidth>Learn More</Button>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Grid>
 
-        <Grid item xs={12} md={4}>
-          <Card variant="outlined" elevation={4} sx={{ "&:hover": { transform: "scale(1.05)", transition: "0.3s" } }}>
-            <CardActionArea>
-              <CardMedia component={GiPaintBrush} height="140" />
-              <CardContent>
-                <Typography variant="h5" align="center" gutterBottom>
-                  Wooden Art Pieces
-                </Typography>
-                <Typography align="center" paragraph>
-                  Discover unique art pieces, handcrafted from wood, perfect for any space.
-                </Typography>
-                <Button variant="contained" color="primary" fullWidth>View Collection</Button>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
+    <Grid item xs={12} md={4}>
+  <Card variant="outlined" elevation={4} sx={{ "&:hover": { transform: "scale(1.05)", transition: "0.3s" } }}>
+    <CardActionArea>
+      <CardMedia component={GiPaintBrush} height="200" /> {/* Increased height */}
+      <CardContent sx={{ padding: '24px' }}> {/* Increased padding */}
+        <Typography variant="h4" align="center" gutterBottom> {/* Increased font size */}
+          Wooden Art Pieces
+        </Typography>
+        <Typography align="center" paragraph style={{ fontSize: '18px' }}> {/* Increased font size */}
+          Discover unique art pieces, handcrafted from wood, perfect for any space.
+        </Typography>
+        <Button variant="contained" color="primary" fullWidth>View Collection</Button>
+      </CardContent>
+    </CardActionArea>
+  </Card>
+</Grid>
 
-        <Grid item xs={12} md={4}>
-          <Card variant="outlined" elevation={4} sx={{ "&:hover": { transform: "scale(1.05)", transition: "0.3s" } }}>
-            <CardActionArea>
-              <CardMedia component={GiDeadWood} height="140" />
-              <CardContent>
-                <Typography variant="h5" align="center" gutterBottom>
-                  Custom Woodwork
-                </Typography>
-                <Typography align="center" paragraph>
-                  From furniture to decor, we can craft custom woodwork tailored to your needs.
-                </Typography>
-                <Button variant="contained" color="primary" fullWidth>Get a Quote</Button>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
+
+<Grid item xs={12} md={4}>
+  <Card variant="outlined" elevation={4} sx={{ "&:hover": { transform: "scale(1.05)", transition: "0.3s" } }}>
+    <CardActionArea>
+      <CardMedia component={GiDeadWood} height="200" /> {/* Increased height */}
+      <CardContent sx={{ padding: '24px' }}> {/* Increased padding */}
+        <Typography variant="h4" align="center" gutterBottom> {/* Increased font size */}
+          Custom Woodwork
+        </Typography>
+        <Typography align="center" paragraph style={{ fontSize: '18px' }}> {/* Increased font size */}
+          From furniture to decor, we can craft custom woodwork tailored to your needs.
+        </Typography>
+        <Button variant="contained" color="primary" fullWidth>Get a Quote</Button>
+      </CardContent>
+    </CardActionArea>
+  </Card>
+</Grid>
+
 
       </Grid>
     </Container>
